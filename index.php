6979999f3a767258ebccc8f904bbb17af965cafc
Gallow <gallow2142@gmail.com>
2019-04-13 15:42:14 +0200

    adding all files

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>DWMA project</title>
+  <link rel="stylesheet" type="text/css" href="src/styles/app.css">
+  <link rel="stylesheet" type="text/css" href="src/styles/bootstrap.min.css">
+</head>
+<body>
+  <img src="src/assets/images/logo-heaj.png">
+  <h1>Hello world</h1>
+  <?php phpinfo();?>
+
+  <script src="src/scripts/jquery-3.4.0.min.js"></script>
+  <script src="src/scripts/bootstrap.min.js"></script>
+  <script src="src/scripts/app.js"></script>
+</body>
+</html>
