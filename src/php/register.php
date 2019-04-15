258ed5337ebd8112b15388da24ea9867c27103e2
Gallow <gallow2142@gmail.com>
2019-04-15 16:09:22 +0200

    idk

--- src/php/register.php
+++ src/php/register.php
@@ -29,45 +29,43 @@
         $user_check_query->execute();
     
         $result = $user_check_query->get_result();
-        if($result->num_rows === 0) exit('No rows');
-        $row = $result->fetch_assoc();
 
-        $user = $row['USERNAME'];
+        //check if exist
+        if($result->num_rows > 0){
+            
 
-        echo $user;
+        }else{
+            $row = $result->fetch_assoc();
+            $user = $row['USERNAME'];
 
-        $town_check_query = "SELECT ID FROM TOWNS WHERE NAME='$town'";
-        $result_town = mysqli_query($link , $town_check_query);
-        $town_array = mysqli_fetch_assoc($result_town);
-        $townToInsert = $town_array['ID'];
-
-        echo $user;
+            $town_check_query = $link->prepare("SELECT ID FROM TOWNS WHERE NAME= ? ");
+            $town_check_query->bind_param("s", $town);
+            $town_check_query->execute();
         
-        if ($user) {
-            if ($row['USERNAME'] === $username) {
-                array_push($errors, "Username already exists");
+            $result = $town_check_query->get_result();
+    
+            //check if exist
+            if($result->num_rows > 0){
+                $row = $result->fetch_assoc();
+                $townToInsert = $row['ID'];
+            }else{
+                $townToInsert = null;
+                if ($townToInsert == null) { array_push($errors, "town is unknown"); }
             }
-        
-            if ($row['EMAIL'] === $email) {
-                array_push($errors, "email already exists");
-            }
-        }
-      
-        if (count($errors) == 0) {
+
             // Set up avatar
             $firstLetter = strtolower($username[0]);
 
-            echo $username;
-            echo $firstLetter;
+            // echo $username;
+            // echo $firstLetter;
+
             $avatarPath = 'src/assets/img/avatar/default_'.$firstLetter.'.png';
 
             $password = md5($password_1);
             $query = "INSERT INTO USER (USERNAME, EMAIL, TOWN_ID, PASSWORD, AVATAR) 
-                      VALUES('$username', '$email', '$townToInsert', '$password', '$avatarPath')";
+                        VALUES('$username', '$email', '$townToInsert', '$password', '$avatarPath')";
             mysqli_query($link, $query);
             $id = mysqli_insert_id($link);
-            // $_SESSION['username'] = $username;
-            // $_SESSION['success'] = "You are now logged in";
 
             // create user directory
             $newDirectory = "../../users/" . $id . "_" . $username;
