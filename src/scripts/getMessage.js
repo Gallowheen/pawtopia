768023380173debecc130bbb08e8490b5d366914
Disgallion <disgallion@gmail.com>
2019-07-06 00:19:46 +0200

    On transforme du code doublon en fonction

--- src/scripts/getMessage.js
+++ src/scripts/getMessage.js
@@ -1,45 +1,5 @@
 $(document).ready(function() {
 
-    $('#message').keypress(function (e) {
-        var key = e.which;
-        if(key == 13)  // the enter key code
-        {
-            if($('#message').val() != ""){
-                $.ajax({
-                    method: "GET",
-                    data: {ID:user2,MESSAGE:$('#message').val()},
-                    url:"src/php/insertMessage.php",
-                })
-                .done(function(result){
-
-                    $.ajax({
-                        method: "GET",
-                        data: {ID:user2},
-                        url:"src/php/getLastMessage.php",
-                    })
-                    .done(function(result){
-
-                        setTimeout(function(){
-                            $('.messages').append(result);
-                        },500);
-
-                        $('.messages').animate({
-                            scrollTop: realHeight
-                        }, 500);
-
-                        if($('#message').hasClass('-error')){
-                            $('#message').removeClass('-error');
-                        }
-
-                        $('#message').val('');
-                    })
-                })
-            }else{
-                $('#message').addClass('-error');
-            }
-        }
-    });
-
     var pubnub = new PubNub({
 	    subscribeKey: 'sub-c-1ef083d0-dc62-11e8-911d-e217929ad048', // always required
 	    publishKey: 'pub-c-07c00e21-e522-43ef-965d-f81e60f7a47f' // only required if publishing
@@ -94,40 +54,52 @@ $(document).ready(function() {
         }, 500);
     },500);
 
-    $('.sendMessage').click(function(){
+    $('#message').keypress(function (e) {
+        var key = e.which;
+        if(key == 13)  // the enter key code
+        {
+        	sendMessage();
+        }
+    });
 
-        if($('#message').val() != ""){
+    $('.sendMessage').click(function(){
+    	sendMessage();
+    });
+});
+
+function sendMessage()
+{
+    if($('#message').val() != ""){
+        $.ajax({
+            method: "GET",
+            data: {ID:user2,MESSAGE:$('#message').val()},
+            url:"src/php/insertMessage.php",
+        })
+        .done(function(result){
+
             $.ajax({
                 method: "GET",
-                data: {ID:user2,MESSAGE:$('#message').val()},
-                url:"src/php/insertMessage.php",
+                data: {ID:user2},
+                url:"src/php/getLastMessage.php",
             })
             .done(function(result){
 
-                $.ajax({
-                    method: "GET",
-                    data: {ID:user2},
-                    url:"src/php/getLastMessage.php",
-                })
-                .done(function(result){
+                setTimeout(function(){
+                    $('.messages').append(result);
+                },500);
 
-                    setTimeout(function(){
-                        $('.messages').append(result);
-                    },500);
+                $('.messages').animate({
+                    scrollTop: realHeight
+                }, 500);
 
-                    $('.messages').animate({
-                        scrollTop: realHeight
-                    }, 500);
+                if($('#message').hasClass('-error')){
+                    $('#message').removeClass('-error');
+                }
 
-                    if($('#message').hasClass('-error')){
-                        $('#message').removeClass('-error');
-                    }
-
-                    $('#message').val('');
-                })
+                $('#message').val('');
             })
-        }else{
-            $('#message').addClass('-error');
-        }
-    });
-});
+        })
+    }else{
+        $('#message').addClass('-error');
+    }
+}
