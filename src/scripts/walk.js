000cedba59ad43db9ec8de03872c0da66f869260
Disgallion <disgallion@gmail.com>
2019-07-05 23:53:31 +0200

    On déplace du code restant pour walk

--- src/scripts/walk.js
+++ src/scripts/walk.js
@@ -251,5 +251,175 @@ $(document).ready(function(){
             });
         });
 
-        $('.map__container').eq(0).show();
+    $('#submit__walks').click(function(e){
+
+        e.preventDefault();
+
+        //Si la géoloc ne fonctionne pas ou n'est pas activ
+        if (latUser == undefined || lonUser == undefined){
+            $.ajax({
+                method: "GET",
+                url:"src/php/get_lat_long_city.php",
+            })
+            .done(function(result){
+
+                data = JSON.parse(result);
+
+                latUser  = data.LAT;
+                lonUser  = data.LON;
+            });
+        }
+
+        setTimeout(function(){
+
+            $.ajax({
+                method: "GET",
+                data:{
+                    walk:$('form input[type=radio]:checked').val(),
+                    //km : $('#slider').val(),
+                    date : $('#date').val(),
+                    LAT : latUser,
+                    LON : lonUser
+                },
+                url:"src/php/managewalk.php",
+            })
+            .done(function(result){
+
+                console.log(result);
+                data = JSON.parse(result);
+                //console.log(result);
+
+                //map.removeLayer();
+
+                //savoir le jour
+                var jour = new Array(7);
+                jour[0] = "Lundi";
+                jour[1] = "Mardi";
+                jour[2] = "Mercredi";
+                jour[3] = "Jeudi";
+                jour[4] = "Vendredi";
+                jour[5] = "Samedi";
+                jour[6] = "Dimanche";
+
+                //savoir le mois
+                var mois = new Array(12);
+                mois[0] = "Janvier";
+                mois[1] = "Février";
+                mois[2] = "Mars";
+                mois[3] = "Avril";
+                mois[4] = "Mai";
+                mois[5] = "Juin";
+                mois[6] = "Juillet";
+                mois[7] = "Août";
+                mois[8] = "Septembre";
+                mois[9] = "Octobre";
+                mois[10] = "Novembre";
+                mois[11] = "Décembre";
+
+                if($('.walk__container__result'))
+                    $('.walk__container__result').remove();
+
+                if($('.tag_container'))
+                    $('.tag_container').remove();
+
+                if($('.walk__noresult'))
+                    $('.walk__noresult').remove();
+
+                if (data.length >= 1){
+
+                    setTimeout(() => {
+                        mymap.invalidateSize();
+                    }, 0);
+                    layerGroup.clearLayers();
+
+                    $('.content_container .container .row .col').append('<div class="walk__container__result"></div>');
+
+                    $('.walk__container__result').append('<p class="search__result"><b>'+data.length+'</b> résultats correspondant à votre recherche</p>');
+
+                    for( let i = 0; i < data.length; i++){
+
+                        let adress = data[i]['LOCATION'];
+
+                        setTimeout(function(){
+                            if (data[i]['WALK'] == "Récréative"){
+                                var marker = L.marker([data[i]['LAT'], data[i]['LON']],{icon: bluetopia}).addTo(mymap).on('click', function(){
+
+                                    setTimeout(function(){
+                                        $('.get_to_walk').click(function(){
+                                            window.location = "walk_detail?ID="+data[i]['ID'];
+                                        });
+                                    },100);
+                                });
+                                marker.bindPopup("<b>"+data[i]['NAME']+"</b></br><b>"+data[i]['ROAD']+" "+data[i]['CITY']+"</b></br><button class='button -color -blue -round -top -walk get_to_walk' data-id='10'>En savoir plus</button>");
+                                marker.addTo(layerGroup);
+                            }
+                            if (data[i]['WALK'] == "Sportive"){
+                                var marker = L.marker([data[i]['LAT'], data[i]['LON']],{icon: redtopia}).addTo(mymap).on('click', function(){
+
+                                    setTimeout(function(){
+                                        $('.get_to_walk').click(function(){
+                                            window.location = "walk_detail?ID="+data[i]['ID'];
+                                        });
+                                    },100);
+                                });
+                                marker.bindPopup("<b>"+data[i]['NAME']+"</b></br><b>"+data[i]['ROAD']+" "+data[i]['CITY']+"</b></br><button class='button -color -blue -round -top -walk get_to_walk' data-id='10'>En savoir plus</button>");
+                                marker.addTo(layerGroup);
+
+                            }
+                            if (data[i]['WALK'] == "Découverte"){
+                                var marker = L.marker([data[i]['LAT'], data[i]['LON']],{icon: greentopia}).addTo(mymap).on('click', function(){
+
+                                    setTimeout(function(){
+                                        $('.get_to_walk').click(function(){
+                                            window.location = "walk_detail?ID="+data[i]['ID'];
+                                        });
+                                    },100);
+                                });
+                                marker.bindPopup("<b>"+data[i]['NAME']+"</b></br><b>"+data[i]['ROAD']+" "+data[i]['CITY']+"</b></br><button class='button -color -blue -round -top -walk get_to_walk' data-id='10'>En savoir plus</button>");
+                                marker.addTo(layerGroup);
+                            }
+                        },250);
+
+                        let date = new Date(data[i]['DATE_START']);
+                        let hourSplit = data[i]['DATE_START'].split(' ')[1].split(':');
+                        let hour = hourSplit[0] + ":" + hourSplit[1];
+                        let day = jour[date.getDay()];
+                        let month = mois[date.getMonth()];
+                        let dayNumber = date.getDate();
+
+                        let walk = '<div class="name__container"><span class="">'+data[i]['NAME']+'</span></div><div class="date__container -flex"><span class="">'+ day +" "+dayNumber +" "+ month+" "+ hour+'</span>'+'<span class="walk__name">'+data[i]['LENGTH']+' heures</span><span>'+data[i]['WALK']+'</span></div><div class="town__container"><i class="icon home icon-ic_home_48px"></i><span class="">'+data[i]['ROAD']+" "+data[i]['CITY']+' <small class="friend_name -km -walk">('+data[i]['km']+' km de vous)</small></span></div><div class="button__container"><button class="button -color -blue -walk get_to_walk" data-id='+data[i]['ID']+'>En savoir plus</button></div>';
+                        $('.walk__container__result').append('<div class="walk__card test">'+walk+'</div>');
+                    }
+                }else{
+                    $('.content_container .container .row .col').append('<div class="walk__noresult">Aucun resultat pour la recherche</div>');
+                }
+
+                $(".walk__handler__container").css('background','transparent');
+
+                setTimeout(function(){
+                    $(".walk__handler__container").css('transform','translateY(100%)');
+                }, 750);
+
+                setTimeout(function(){
+                    $(".walk__handler__container").css('display','none');
+                    $("body").css('overflow','auto');
+
+                    $([document.documentElement, document.body]).animate({
+                        scrollTop: $("#mapid").offset().top - 150
+                    }, 500);
+                }, 1250);
+
+                //$(".map__container").show();
+
+                $('.map__container').eq(0).hide();
+                $(".map__container").eq(1).show();
+                $('.get_to_walk').click(function(){
+                    window.location = "walk_detail?ID="+$(this).data('id');
+                });
+            });
+
+        },500);
+    });
+
+    $('.map__container').eq(0).show();
 });
