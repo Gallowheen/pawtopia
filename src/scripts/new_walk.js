60a2fb28d063fb4d9f05d578da85063b99b0ab2e
Disgallion <disgallion@gmail.com>
2019-07-05 22:48:57 +0200

    On déplace le code du formulaire d'ajout de balade

--- src/scripts/new_walk.js
+++ src/scripts/new_walk.js
@@ -59,35 +59,120 @@ $(document).ready(function() {
                 $("#validate").prop('disabled', true);
             }
 
-            if($('body').is('.new_walk') || $('body').is('.walk_detail')){
-                $('.dog_card').click(function(){
-                    if(!$(this).hasClass('-active')){
-                        $(this).addClass('-active');
-                    }else{
-                        $(this).removeClass('-active');
+            $('.dog_card').click(function(){
+                if(!$(this).hasClass('-active')){
+                    $(this).addClass('-active');
+                }else{
+                    $(this).removeClass('-active');
+                }
+
+                var selected = false;
+
+                for(i = 0; i < $('.dog_card').length && !selected; i++){
+                    if ($('.dog_card').eq(i).hasClass('-active')){
+                        selected = true;
                     }
-
-                    var selected = false;
-
-                    for(i = 0; i < $('.dog_card').length && !selected; i++){
-                        if ($('.dog_card').eq(i).hasClass('-active')){
-                            selected = true;
-                        }
-                    }
-                    //console.log(selected);
-                    if(!selected)
-                        $("#validate__sign").prop('disabled', true);
-                    else
-                        $("#validate__sign").prop('disabled', false);
-                })
-            }
-
-            if($('body').is('.walk_detail')){
-                //console.log('lol');
-                $("#validate__sign").prop('disabled', true);
-                $('.submit__button').remove();
-                $(window).scrollTop($('.walk__add__dog').offset().top);
-            }
+                }
+                //console.log(selected);
+                if(!selected)
+                    $("#validate__sign").prop('disabled', true);
+                else
+                    $("#validate__sign").prop('disabled', false);
+            });
         }
     });
+
+    $('#validate__sign').click(function(e){
+
+        let dogSelected = [];
+        let id_event = $('#validate__sign').data('id');
+        let x = 0;
+
+        $('.dog_card').each(function(){
+            if($(this).hasClass('-active')){
+                dogSelected.push($(this).data('id'));
+            }
+        });
+
+        if(dogSelected.length > 0){
+            $.ajax({
+                method: "GET",
+                data:{
+                   DOG : dogSelected,
+                   ID : id_event
+                },
+                url:"src/php/joinwalk.php",
+            })
+            .done(function(result){
+                if (result == "success")
+                    document.location.reload(true);
+            });
+        }
+    });
+
+    $("#validate").click(function(e){
+        e.preventDefault();
+
+        var date = $("#date").val() + " " + $("#time").val();
+
+        let dogSelected = [];
+        let x = 0;
+
+        $('.dog_card').each(function(){
+            if($(this).hasClass('-active')){
+                dogSelected.push($(this).data('id'));
+            }
+        });
+
+        $.get(location.protocol + '//nominatim.openstreetmap.org/search?format=json&q='+$('#info').val()+'&addressdetails=1', function(data){
+            console.log(data[0]);
+            lat = data[0].lat;
+            lon = data[0].lon;
+
+            if (data[0].address.city != undefined){
+                city = data[0].address.city;
+            }else{
+                city = data[0].address.village;
+            }
+            road = data[0].address.road;
+            postcode = data[0].address.postcode;
+        });
+
+        setTimeout(function(){
+            console.log(lat);
+            console.log(lon);
+            console.log(city);
+            console.log(road);
+            console.log(postcode);
+
+
+            $.get(
+                'src/php/add_event.php',
+                {
+                    ID_OWNER : $("form").data('id'),
+                    NAME : $("#walk_name").val(),
+                    LOCATION : $("#info").val(),
+                    TYPE : $("#walk_type").val(),
+                    DATE : date,
+                    LENGTH : $("#length").val(),
+                    DOG : dogSelected,
+                    LAT : lat,
+                    LON : lon,
+                    CITY : city,
+                    ROAD : road,
+                    POSTCODE : postcode
+                },
+
+                function(data){
+                    console.log(data);
+                    if(data === 'success'){
+                        window.location = "walk.php";
+                    }
+                },
+                'text'
+            );
+        },500);
+
+    });
+
 });
