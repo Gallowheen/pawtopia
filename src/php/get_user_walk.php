c7baed298b92338ac5b97eeef0c19338cf603b27
Disgallion <disgallion@gmail.com>
2019-07-06 10:29:51 +0200

    On enlève un l'ajax inutile pour get_user_walk
    On déplace le html généré par le javascript directement dans le php pour éviter d'avoir une requête ajax juste après le chargement

--- src/php/get_user_walk.php
+++ src/php/get_user_walk.php
@@ -2,18 +2,10 @@
     require_once("bdd.php");
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
-    session_start();
+    @session_start();
     $link = mysqli_connect(HOST, USER, PWD, BASE);
     mysqli_query($link, "SET NAMES UTF8");
-
-    //Function to return table of result
-    function resultToArray($result) {
-        $rows = array();
-        while($row = $result->fetch_assoc()) {
-            $rows[] = $row;
-        }
-        return $rows;
-    }
+    $page = (isset($_GET['page']) ? $_GET['page'] : "home");
 
     //$town1 = $_SESSION['TOWN_ID'];
 
@@ -32,8 +24,14 @@
     $query->execute();
 
     $result = $query->get_result();
+    $html = "";
     if($result->num_rows === 0){
-
+        if($page == "home") {
+            $html .= "<p class='information'>Vous n'êtes inscrit à aucune balades pour le moment !</p><div class='center'><a href='walk.php'><button class='button -color'>Découvrez les balades</button></a></div>";
+        }
+        else {
+            $html .= "<img class='map__img' src='src/assets/img/ressources/no_walk.png'/>";
+        }
     }else{
         $rows = resultToArray($result);
 
@@ -83,6 +81,72 @@
         //     $valid_event[$cle]['town_name'] = $town_name[$cle];
         // }
 
-        echo json_encode($valid_event);
+        // echo json_encode($valid_event);
+        $data = $valid_event;
+
+        //savoir le jour
+        $jour = [
+            0 => "Lundi",
+            1 => "Mardi",
+            2 => "Mercredi",
+            3 => "Jeudi",
+            4 => "Vendredi",
+            5 => "Samedi",
+            6 => "Dimanche",
+        ];
+
+        //savoir le mois
+        $mois = [
+            0 => "Janvier",
+            1 => "Février",
+            2 => "Mars",
+            3 => "Avril",
+            4 => "Mai",
+            5 => "Juin",
+            6 => "Juillet",
+            7 => "Août",
+            8 => "Septembre",
+            9 => "Octobre",
+            10 => "Novembre",
+            11 => "Décembre",
+        ];
+
+        if (count($data)){
+
+            $html .= '<div class="walk__container">';
+
+            for($i = 0; $i < count($data); $i++){
+                // Ceci ne marchera pas en php, les dates sont gérées autrement, fix dans le prochain commit
+
+                /*$date = new Date($data[$i]['DATE_START']);
+                $hourSplit = explode(':', explode(' ', $data[$i]['DATE_START'][1]));
+                $hour = $hourSplit[0] . ":" . $hourSplit[1];
+                $day = $jour[$date.getDay()];
+                $monthNumber = $date.getMonth()+1;
+                $month = $mois[$date.getMonth()];
+                $dayNumber = $date.getDate();
+                $year = $date.getFullYear();
+                //$day . " " . $dayNumber . " " . $month . " " . $hour
+                */
+                $date = '';
+
+                $walk = '<div class="name__container">
+                            <span>' . $data[$i]['NAME'] . '</span>
+                        </div>
+                        <div class="address__container">' . $data[$i]['ROAD'] . ' ' . $data[$i]['CITY'] . '</div>
+                        <div class="date__container">
+                            <span class="">' . $date . '</span>
+                        </div>
+                        <div class="button__container -preview">
+                            <i class="icon icon__up -right get_to_walk" data-id=' . $data[$i]['ID'] . '></i>
+                            <span class="learn__more">En savoir plus</span>
+                        </div>';
+                // A rajouter à la ligne du dessus pour repasser à l'ancienne version
+                //+'<span class="walk__name">'+data[i]['LENGTH']+' heures</span><span>'+data[i]['WALK']+'</span></div><div class="town__container"><i class="icon home icon-ic_home_48px"></i><span class="">'+data[i]['LOCATION']+'</span></div><div class="align-right"><div class="button__container"><button class="button -color -blue -round -walk get_to_walk" data-id='+data[i]['ID']+'>En savoir plus</button></div></div>'
+                $html .= '<div class="walk__card -home test">' . $walk . '</div>';
+            }
+            $html .= '</div>';
+        }
     }
+    echo $html;
 ?>
