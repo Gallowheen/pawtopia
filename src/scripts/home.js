e2746b381d6929b3329518fd465b88a50c1f8f47
Disgallion <disgallion@gmail.com>
2019-07-07 20:26:57 +0200

    On créer une fonction de retour et on l'applique aux sous pages de home

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $('.get_to_walk').click(function(){
+        var title = $(".header__title").html();
         $.ajax({
             method: "GET",
             url:"walk_detail.php",
@@ -8,26 +9,31 @@ $(document).ready(function() {
         .done(function(result){
             container.html(result);
             $('.h1').text('Balades');
+            setReturnButton("home", {}, title);
         });
     });
 
     $('.statuts .icon__friend').click(function(){
+        var title = $(".header__title").html();
         $.ajax({
             url:"friends.php",
         })
         .done(function(result){
             container.html(result);
             $('.h1').text('Amis');
+            setReturnButton("home", {}, title);
         });
     });
 
     $('.statuts .icon__message').click(function(){
+        var title = $(".header__title").html();
         $.ajax({
             url:"message.php",
         })
         .done(function(result){
             container.html(result);
             $('.h1').text('Messages');
+            setReturnButton("home", {}, title);
         });
     });
 })
